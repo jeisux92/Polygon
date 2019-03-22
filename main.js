fd836c39c71f1a922dae63f0eaa156519887afc7
Gabriel Martinez <31553757+jeisux92@users.noreply.github.com>
2019-03-21 20:46:28 -0500

    Phone test

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.querySelector("#polygon").addEventListener("mousedown",(e)=>{
   })
 })
 
-document.querySelector("#polygon").addEventListener("mousemove",(e)=>{
+document.querySelector("#polygon").addEventListener("mousemove touchmove",(e)=>{
       if(!point){
         return;
       }
